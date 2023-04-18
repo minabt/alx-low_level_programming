c8427ded132c7d7f6576dde383d03ee853f58745
minabt <minasamia26@gmail.com>
2023-04-17 17:21:47 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog info
+ * @name: name of dog
+ * @age: dog age
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
